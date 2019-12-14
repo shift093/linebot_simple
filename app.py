98edcf266f897253f1de23c3b6b8db908da99d62
Setup <33972291+shift093@users.noreply.github.com>
2019-12-15 03:36:57 +0800

    Update app.py

--- app.py
+++ app.py
@@ -36,10 +36,10 @@ def handle_message(event):
     message = TextSendMessage(text=event.message.text)
     line_bot_api.reply_message(event.reply_token, message)
 
-#import os
-#if __name__ == "__main__":
-#    port = int(os.environ.get('PORT', 5000))
-#    app.run(host='0.0.0.0', port=port)
-    
+import os
 if __name__ == "__main__":
-    app.run()    
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
+    
+#if __name__ == "__main__":
+#    app.run()    
